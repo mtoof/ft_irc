8f3045fe4be475491657f2e6b6241ff2a3ebe96b
trsctr <trisector@gmail.com>
2024-05-18 22:50:55 +0300

    cleaned commandpass

--- src/command/CommandPass.cpp
+++ src/command/CommandPass.cpp
@@ -3,19 +3,19 @@
 void Command::handlePass(const Message &msg)
 {
 	std::shared_ptr<Client> client_ptr = msg.getClientPtr();
-	int fd = client_ptr->getFd();
+	int client_fd = client_ptr->getFd();
 	if (server_ptr_->getPassword().empty())
 		return;
 	if (client_ptr->getRegisterStatus() == true)
 	{
-		server_ptr_->send_response(fd, ERR_ALREADYREGISTERED(server_ptr_->getServerHostname(), client_ptr->getNickname()));
+		server_ptr_->send_response(client_fd, ERR_ALREADYREGISTERED(server_ptr_->getServerHostname(), client_ptr->getNickname()));
 		return;
 	}
 	std::vector<std::string> parameters = msg.getParameters();
 	size_t pos = parameters.front().find_first_not_of(" \t\v");
 
 	if (pos == std::string::npos || parameters.empty())
-		server_ptr_->send_response(msg.getClientfd(), ERR_NEEDMOREPARAMS(std::string("*"), "PASS"));
+		server_ptr_->send_response(client_fd, ERR_NEEDMOREPARAMS(std::string("*"), "PASS"));
 	else if (!client_ptr->getRegisterStatus())
 	{
 		if (parameters.front() == server_ptr_->getPassword())
@@ -25,10 +25,10 @@ void Command::handlePass(const Message &msg)
 		}
 		else if (!server_ptr_->getPassword().empty() && parameters.front() != server_ptr_->getPassword())
 		{
-			server_ptr_->send_response(fd, ERR_INCORPASS(server_ptr_->getServerHostname(), client_ptr->getNickname()));
-			server_ptr_->send_response(fd, "Connection got rejected by the server\r\n");
-			server_ptr_->closeDeletePollFd(fd);
-			server_ptr_->deleteClient(fd);
+			server_ptr_->send_response(client_fd, ERR_INCORPASS(server_ptr_->getServerHostname(), client_ptr->getNickname()));
+			server_ptr_->send_response(client_fd, "Connection got rejected by the server\r\n");
+			server_ptr_->closeDeletePollFd(client_fd);
+			server_ptr_->deleteClient(client_fd);
 		}
 	}
 }
