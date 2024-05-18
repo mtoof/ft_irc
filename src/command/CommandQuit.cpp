904e5407db82390643e93e7772479ae696e620b4
trsctr <trisector@gmail.com>
2024-05-18 22:53:52 +0300

    cleaned commandquit

--- src/command/CommandQuit.cpp
+++ src/command/CommandQuit.cpp
@@ -3,27 +3,27 @@
 void Command::handleQuit(const Message &msg)
 {
 	std::shared_ptr<Client> client_ptr = msg.getClientPtr();
-	int fd = client_ptr->getFd();
+	int client_fd = client_ptr->getFd();
 	std::vector<std::shared_ptr<Channel>> channel_list = client_ptr->getChannels();
 	std::string reason = msg.getTrailer().empty() ? "Leaving" : msg.getTrailer(); // default part message is the nickname
-	server_ptr_->send_response(fd, "ERROR: Bye, see you soon!\r\n");	
+	server_ptr_->send_response(client_fd, "ERROR: Bye, see you soon!\r\n");	
 	if (!channel_list.empty())
 	{
 		std::vector<int> fds_sent_to = {};
-		for(auto channel : channel_list)
+		for(auto channel_ptr : channel_list)
 		{
-			for (const auto &userPair : channel->getUsers())
+			for (const auto &recipient_pair : channel_ptr->getUsers())
 			{
-				std::shared_ptr<Client> user = userPair.first;
-				int user_fd = user->getFd();
-				auto it = std::find(fds_sent_to.begin(), fds_sent_to.end(), user_fd);
-				if (user != client_ptr && it == fds_sent_to.end()) // Don't send the message to the sender
+				std::shared_ptr<Client> recipient_ptr = recipient_pair.first;
+				int recipient_fd = recipient_ptr->getFd();
+				auto it = std::find(fds_sent_to.begin(), fds_sent_to.end(), recipient_fd);
+				if (recipient_ptr != client_ptr && it == fds_sent_to.end()) // Don't send the message to the sender
 				{
-					server_ptr_->send_response(user_fd, RPL_QUIT(client_ptr->getClientPrefix(), reason));
-					fds_sent_to.push_back(user_fd);
+					server_ptr_->send_response(recipient_fd, RPL_QUIT(client_ptr->getClientPrefix(), reason));
+					fds_sent_to.push_back(recipient_fd);
 				}
 			}
-			channel->removeUser(client_ptr);
+			channel_ptr->removeUser(client_ptr);
 		}
 	}
 	server_ptr_->disconnectAndDeleteClient(client_ptr);
